787dd8070b99c6d76e074e4aaecb3b7888374663
MrPokemon11 <53610352+MrPokemon11@users.noreply.github.com>
2025-12-09 12:56:55 -0500

    add objects

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,14 +1,21 @@
+using System;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
     public float speed = 5f; // Movement speed
+    public float jumpForce = 3f; // Jump force
 
     void Update()
     {
         // Get input values
         float horizontal = Input.GetAxis("Horizontal"); // A/D or Left/Right
         float vertical = Input.GetAxis("Vertical");     // W/S or Up/Down
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            gameObject.GetComponent<Rigidbody>().AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
 
         // Movement direction
         Vector3 direction = new Vector3(horizontal, 0f, vertical);
